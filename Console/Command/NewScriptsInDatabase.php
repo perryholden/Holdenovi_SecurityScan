ffa58af3314a97a0bf0f35d06c56c8d1b5e98fa2
Perry Holden <perry@krakencommerce.com>
2021-06-12 14:45:17 -0500

    Successfully write current status to database

--- Console/Command/NewScriptsInDatabase.php
+++ Console/Command/NewScriptsInDatabase.php
@@ -7,21 +7,51 @@
  */
 namespace Holdenovi\SecurityScan\Console\Command;
 
+use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Directory\ReadInterface;
+use Magento\Framework\Filesystem\Directory\WriteInterface;
+use Magento\Framework\Serialize\Serializer\Json;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class NewScriptsInDatabase extends Command
 {
     /**
+     * Used for setting or resetting the initial status file
+     */
+    protected const SET_STATUS = 'set-status';
+
+    /**
+     * Name of the directory under var in which the status file will be saved
+     */
+    protected const FOLDER_PATH = 'scan';
+
+    /**
+     * Name of the status file
+     */
+    protected const STATUS_FILE_NAME = 'status.json';
+
+    /**
      * Array of table names and their fields to search
      */
     protected const DEFAULT_SCAN_TABLES = [
-        'core_config_data' => ['path', 'value'],
-        'cms_block' => ['content'],
-        'cms_page' => ['content'],
+        'core_config_data' => [
+            'key' => 'config_id',
+            'search' => ['value']
+        ],
+        'cms_block' => [
+            'key' => 'block_id',
+            'search' => ['content']
+        ],
+        'cms_page' => [
+            'key' => 'page_id',
+            'search' => ['content', 'layout_update_xml']
+        ]
     ];
 
     /**
@@ -29,11 +59,31 @@ class NewScriptsInDatabase extends Command
      */
     protected $resourceConnection;
 
+    /**
+     * @var Filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * @var Json
+     */
+    protected $json;
+
+    /**
+     * @param ResourceConnection $resourceConnection
+     * @param Filesystem $filesystem
+     * @param Json $json
+     * @param string|null $name
+     */
     public function __construct(
         ResourceConnection $resourceConnection,
+        Filesystem $filesystem,
+        Json $json,
         string $name = null
     ) {
         $this->resourceConnection = $resourceConnection;
+        $this->filesystem = $filesystem;
+        $this->json = $json;
         parent::__construct($name);
     }
 
@@ -44,6 +94,12 @@ class NewScriptsInDatabase extends Command
     {
         $this->setName('holdenovi:scan:database');
         $this->setDescription('Scans predefined database tables for new JS scripts.');
+        $this->addOption(
+            self::SET_STATUS,
+            null,
+            InputOption::VALUE_NONE,
+            'Run with this flag to set or reset the status file'
+        );
         parent::configure();
     }
 
@@ -59,42 +115,90 @@ class NewScriptsInDatabase extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $statusOutput = [];
+        $setStatus = $input->getOption(self::SET_STATUS);
+
         /** @var AdapterInterface $connection */
         $connection = $this->resourceConnection->getConnection();
 
         foreach (self::DEFAULT_SCAN_TABLES as $tableName => $tableFields) {
 
             $tableName = $connection->getTableName($tableName);
-            $tableFieldsString = implode(', ', $tableFields);
-            if (count($tableFields) > 1) {
-                $queryFormat = "SELECT %s FROM %s WHERE CONCAT_WS(%s) LIKE '%%script%%'";
+            $tableFieldsString = implode(', ', $tableFields['search']);
+
+            // Setup query based on number of search fields
+            if (count($tableFields['search']) > 1) {
+                $queryFormat = "SELECT %s, %s FROM %s WHERE CONCAT_WS(%s) LIKE '%%script%%'";
             } else {
-                $queryFormat = "SELECT %s FROM %s WHERE %s LIKE '%%script%%'";
+                $queryFormat = "SELECT %s, %s FROM %s WHERE %s LIKE '%%script%%'";
             }
-            $query = sprintf($queryFormat, $tableFieldsString, $tableName, $tableFieldsString);
+            $query = sprintf($queryFormat, $tableFields['key'], $tableFieldsString, $tableName, $tableFieldsString);
             $results = $connection->fetchAll($query);
 
+            // Search through all returned rows
             foreach ($results as $result) {
 
-                foreach ($result as $column) {
+                // Search through columns for script tags
+                foreach ($result as $columnId => $columnValue) {
 
-                    preg_match_all('#<script.*?</script>#is', $column, $matches);
+                    // Skip searching table identifier, but we will use its value in reporting below
+                    if ($tableFields['key'] === $columnId) {
+                        continue;
+                    }
+
+                    // Search for script tags and pull values for hashing
+                    preg_match_all('#<script.*?</script>#is', $columnValue, $matches);
 
                     foreach ($matches as $match) {
                         if (!empty($match)) {
 
-                            $scriptText = $match[0];
-                            $hashText = md5($scriptText);
+                            foreach ($match as $singleMatch) {
 
-                            $output->writeln("<info>$scriptText</info>");
-                            $output->writeln("<error>$hashText</error>");
+                                $hashText = md5($singleMatch);
+                                $identifierValue = $result[$tableFields['key']];
+
+                                $statusOutput[$tableName][$identifierValue][$columnId][] = $hashText;
+                            }
                         }
                     }
-
                 }
             }
         }
 
-        $output->writeln('<info>Scan complete</info>');
+        $processResult = $this->processResults($statusOutput, $setStatus);
+
+        $output->writeln("<info>$processResult</info>");
+    }
+
+    /**
+     * @param array $statusOutput
+     * @param boolean $setStatus
+     * @return string
+     * @throws \Magento\Framework\Exception\FileSystemException
+     */
+    protected function processResults($statusOutput, $setStatus)
+    {
+        $serializedOutput = $this->json->serialize($statusOutput);
+
+        // If "set-status", then merely write the file for use in future scans
+        if ($setStatus) {
+
+            /** @var WriteInterface $writeDirectory */
+            $writeDirectory = $this->filesystem->getDirectoryWrite(DirectoryList::VAR_DIR);
+            $writeDirectory->writeFile(self::FOLDER_PATH . DS . self::STATUS_FILE_NAME, $serializedOutput);
+
+            return 'Status successfully saved';
+
+        }
+
+        // First, we want to get the current values saved in the status file
+        /** @var ReadInterface $readDirectory */
+        $readDirectory = $this->filesystem->getDirectoryRead(DirectoryList::VAR_DIR);
+        $unserializedStatus = $this->json->unserialize($readDirectory->readFile(self::FOLDER_PATH . DS . self::STATUS_FILE_NAME));
+
+        // TODO: Secondly, we want to compare the current status output with the saved status file
+
+        // TODO: Finally, we want to notify the admin of the changes with the config
+
     }
 }
--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Holdenovi_SecurityScan
 
-### TODO:
+## Instructions
 
-1. Test multi-column detection
-2. Save values in var config file
+1. Verify that you have no malware or malicious scripts in the following database tables:
+  * `cms_block`
+  * `cms_page`
+  * `core_config_data`
+2. Run the following command to write the script status config to `var/scan`:
+  * `bin/magento holdenovi:scan:database --set-status`
 
 ### Known Issues
 
 * Cannot find or hash script that exists between two fields.
+* If you use Capistrano, add the `var/scan` folder to your shared directories, otherwise, your status file will lost in subsequent deployments.
+
+## TODO
+
+Add feature to allow additional table configs.
